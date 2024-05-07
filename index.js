0244042d64700ee94aba0c9a2ef69ca1486d79bd
vodapermadi <yapsitusaya111@gmail.com>
2024-05-07 20:55:29 +0700

    disable cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { getData, insertOne, deleteOne, deleteMany, insertMany, updateOne, updateMany } = require('./src/lib/mongodb')
+const cors = require('cors')
 
 const app = express()
 const port = 8000
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
